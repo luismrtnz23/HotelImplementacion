6235c84e3f85bbf51883b6d6abd27d7bf64d8bf6
luismrtnz <luis.martinezplano@gmail.com>
2022-04-21 08:29:13 +0200

    Initial commit

---
+++ src/pC05/Referencia.java
@@ -0,0 +1,19 @@
+package pC05;
+
+public class Referencia {
+    public int x;
+
+    public Referencia() {
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+
+}
+
